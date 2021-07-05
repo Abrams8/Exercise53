e2028e0436049eee262a437ab53d38eea3fa8702
Abrams <dima.abramchik@gmail.com>
2021-07-05 03:04:02 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,29 @@ public class Main {
         threadTwo.setName("ThreadTwo");
         Thread threadThree = new MyThread();
         threadThree.setName("ThreadTree");
+        Thread threadFour = new MyThread();
+        threadFour.setName("ThreadFour");
+        Thread threadFive = new MyThread();
+        threadFive.setName("ThreadFive");
+        Thread threadSix = new MyThread();
+        threadSix.setName("ThreadSix");
+        Thread threadSeven = new MyThread();
+        threadSeven.setName("ThreadSeven");
+        Thread threadEight = new MyThread();
+        threadEight.setName("ThreadEight");
+        Thread threadNine = new MyThread();
+        threadNine.setName("ThreadNine");
+        Thread threadTen = new MyThread();
+        threadTen.setName("ThreadTen");
         threadOne.start();
         threadTwo.start();
         threadThree.start();
+        threadFour.start();
+        threadFive.start();
+        threadSix.start();
+        threadSeven.start();
+        threadEight.start();
+        threadNine.start();
+        threadTen.start();
     }
 }
